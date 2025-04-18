f42524f6f98b8b13cf83716eef23e1981e228400
Nurettin Yıldız <93829462+Nyildizzz@users.noreply.github.com>
2025-04-18 20:51:13 +0300

    feat: add next.config.js and update tsconfig.json to include it

--- lyrics-next-app/next.config.js
+++ lyrics-next-app/next.config.js
@@ -1,11 +1,9 @@
-import type { NextConfig } from "next";
-
 const isProd = process.env.NODE_ENV === "production";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   output: "export",
   basePath: isProd ? "/lyrics-next-app" : "",
   assetPrefix: isProd ? "/lyrics-next-app/" : "",
 };
 
-export default nextConfig;
+module.exports = nextConfig;
